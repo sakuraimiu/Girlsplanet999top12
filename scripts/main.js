b0f7e621991cbab1c820c95408588805e6c300e2
GIRLS PLANET 999 <87678597+sakuraimiu@users.noreply.github.com>
2021-07-29 07:59:14 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -389,7 +389,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://produce48.github.io/";
+const currentURL = "https://sakuraimiu.github.io/Girlsplanet999top12/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
